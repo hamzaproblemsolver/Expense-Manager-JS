2c0644fea18d16ede5fa164e8f918cb871f4abab
Deepanshu Bagri <deepanshu.bagri98@gmail.com>
2017-12-20 20:40:28 +0530

    FIXES

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    port : 5000,
+    port : 4000,
     database : "mongodb://root:abc123@ds159776.mlab.com:59776/expense_manager",
     secretKey : "Secret",
 
@@ -7,13 +7,13 @@ module.exports = {
         clientID        : process.env.FACEBOOK_ID || '315654992265549',
         clientSecret    : process.env.FACEBOOK_SECRET || '332a70053b41adb89a46a0269d851e37',
         profileFields   : ['emails','displayName'],
-        callbackURL     : 'http://localhost:4000/auth/facebook/callback'
+        callbackURL     : '/auth/facebook/callback'
     },
 
     google: {
         clientID        : "19681958519-j3efc8nrefc8v6sp4dt69usd3atinb4p.apps.googleusercontent.com",
         clientSecret    : "ht4jYHmwu1FRxfm1A0ApSLa6",
-        callbackURL     : 'http://localhost:4000/auth/google/callback'
+        callbackURL     : '/auth/google/callback'
     },
 
     mailing :{
