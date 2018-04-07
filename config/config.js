50a22fc301ffd3c8016422cc15d7b5f5ce9a89d4
Deepanshu Bagri <deepanshu.bagri98@gmail.com>
2018-04-07 13:35:48 +0530

    Passbook

--- config/config.js
+++ config/config.js
@@ -17,7 +17,7 @@ module.exports = {
     },
 
     mailing :{
-        from : "develop.bagri98@gmail.com",
-        password : "jaibhagwan"
+        from : "<email>",
+        password : "<password>"
     }
 }
